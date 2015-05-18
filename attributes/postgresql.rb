bb23b98d3f19bc363e7e841bff9382cde37d32c4
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-05-17 22:05:09 -0300

    Add pg_stat_statements to postgresql
    add missing pg_stat_statements

--- attributes/postgresql.rb
+++ attributes/postgresql.rb
@@ -7,3 +7,5 @@ default[:postgresql][:pg_hba] = [
     method: 'trust'
   }
 ]
+default[:postgresql][:config][:shared_preload_libraries] = 'pg_stat_statements'
+default[:postgresql][:contrib][:extensions] = ['pg_stat_statements']
--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -1,1 +1,7 @@
+%w(postgresql-contrib).each do |package_name|
+  package package_name do
+    action :install
+  end
+end
+
 include_recipe 'postgresql::server'
