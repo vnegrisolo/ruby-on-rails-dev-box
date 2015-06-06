493e9d19f598ca40065a07aadaae664b92d34676
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-06-06 11:01:10 -0300

    Configure just the basic recipes
    other dependency recipes will not be available as default.

--- attributes/postgresql.rb
+++ attributes/postgresql.rb
@@ -3,4 +3,4 @@ default[:postgresql][:pg_hba] = [
   { type: 'local', db: 'all', user: 'postgres', method: 'trust' }
 ]
 default[:postgresql][:config][:shared_preload_libraries] = 'pg_stat_statements'
-default[:postgresql][:contrib][:extensions] = ['pg_stat_statements']
+default[:postgresql][:contrib][:extensions] = %w(pg_stat_statements)
