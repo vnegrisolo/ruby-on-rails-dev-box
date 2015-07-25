70e5f5a199951ced6d1678100238d931a8bdb3b5
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-07-24 21:20:34 -0300

    Add vim to the vm

--- recipes/vim_neobundle.rb
+++ recipes/vim_neobundle.rb
@@ -7,6 +7,12 @@
   end
 end
 
+%w(vim).each do |package_name|
+  package package_name do
+    action :install
+  end
+end
+
 git "#{node[:home]}/.vim/bundle/neobundle.vim" do
   user node[:user]
   group node[:group]
