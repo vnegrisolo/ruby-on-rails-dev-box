b4e743fc3c0c0addc1ef102e5bd45130d0f46ca6
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-07-26 00:23:20 -0300

    Update postgresql to 9.4

--- attributes/postgresql.rb
+++ attributes/postgresql.rb
@@ -1,6 +1,38 @@
+default[:postgresql][:version] = '9.4'
+
 default[:postgresql][:password][:postgres] = ''
 default[:postgresql][:pg_hba] = [
   { type: 'local', db: 'all', user: 'postgres', method: 'trust' }
 ]
 default[:postgresql][:config][:shared_preload_libraries] = 'pg_stat_statements'
 default[:postgresql][:contrib][:extensions] = %w(pg_stat_statements)
+
+####################
+# force version 9.4
+####################
+default[:postgresql][:enable_pgdg_apt] = true
+
+# fake recipe to install version not accepted yet
+default[:postgresql][:pgdg][:release_apt_codename] = 'squeeze'
+
+default[:postgresql][:dir] =
+  "/etc/postgresql/#{default[:postgresql][:version]}/main"
+default[:postgresql][:client][:packages] = [
+  "postgresql-client-#{default[:postgresql][:version]}",
+  'libpq-dev'
+]
+default[:postgresql][:server][:packages] = [
+  "postgresql-#{default[:postgresql][:version]}"
+]
+default[:postgresql][:contrib][:packages] = [
+  "postgresql-contrib-#{default[:postgresql][:version]}"
+]
+
+default[:postgresql][:config][:data_directory] =
+  "/var/lib/postgresql/#{default[:postgresql][:version]}/main"
+default[:postgresql][:config][:hba_file] =
+  "/etc/postgresql/#{default[:postgresql][:version]}/main/pg_hba.conf"
+default[:postgresql][:config][:ident_file] =
+  "/etc/postgresql/#{default[:postgresql][:version]}/main/pg_ident.conf"
+default[:postgresql][:config][:external_pid_file] =
+  "/var/run/postgresql/#{default[:postgresql][:version]}-main.pid"
